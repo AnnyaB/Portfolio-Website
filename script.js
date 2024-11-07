dfb54f831a9ef052f58f47d1fe7de3869596218b
Riya <rb23ack@herts.ac.uk>
2024-11-07 19:45:51 +0000

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ function toggleDetails(id) {
     details.style.display = (details.style.display === "block") ? "none" : "block";
 }
 
-// Set project details to be hidden initially
+// Setting project details to be hidden initially
 document.addEventListener("DOMContentLoaded", () => {
     document.querySelectorAll('.project-details').forEach(details => {
         details.style.display = 'none';
